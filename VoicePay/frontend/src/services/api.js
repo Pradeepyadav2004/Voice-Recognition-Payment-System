ac36f61c319eb7356ea1c28165506ec379a13e5f
Pradeep Kumar Yadav <152700585+Pradeepyadav2004@users.noreply.github.com>
2026-05-29 14:36:52 +0530

    Change API base URL to production endpoint

--- VoicePay/frontend/src/services/api.js
+++ VoicePay/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://voicepay-ftbjcaard5dkgxc0.centralindia-01.azurewebsites.net/api',
     headers: {
         'Content-Type': 'application/json',
     },
